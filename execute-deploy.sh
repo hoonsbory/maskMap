06f587fef192964323ee733249d2e9c4ed35ef60
unknown <tonymkcv@naver.com>
2020-03-18 03:13:38 +0900

    도커 자동배포 + 기능 최종수정

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -3,5 +3,5 @@ sudo docker stop maskmap
 sudo docker rm maskmap
 sudo docker rmi maskmap
 
-sudo build -t maskmap .
+sudo docker build -t maskmap .
 sudo docker run -d --name maskmap -p 80:80 maskmap
