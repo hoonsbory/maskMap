0d88110efce33b351618319b79cdb2d980639582
unknown <tonymkcv@naver.com>
2020-03-18 18:35:48 +0900

    포트수정

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-sudo docker stop server1
-sudo docker cp /home/ubuntu/travis/maskMap/target/maskmap-0.0.1-SNAPSHOT.jar server1:/
+# sudo docker stop server1
+# sudo docker cp /home/ubuntu/travis/maskMap/target/maskmap-0.0.1-SNAPSHOT.jar server1:/
 
 
-sudo docker start server1
+# sudo docker start server1
 
-sleep 5
+# sleep 5
 
-sudo docker stop server2
-sudo docker cp /home/ubuntu/travis/maskMap/target/maskmap-0.0.1-SNAPSHOT.jar server2:/
-
-
-sudo docker start server2
-
-sleep 5
-
-sudo docker stop server3
-sudo docker cp /home/ubuntu/travis/maskMap/target/maskmap-0.0.1-SNAPSHOT.jar server3:/
+# sudo docker stop server2
+# sudo docker cp /home/ubuntu/travis/maskMap/target/maskmap-0.0.1-SNAPSHOT.jar server2:/
 
 
-sudo docker start server3
+# sudo docker start server2
+
+# sleep 5
+
+# sudo docker stop server3
+# sudo docker cp /home/ubuntu/travis/maskMap/target/maskmap-0.0.1-SNAPSHOT.jar server3:/
+
+
+# sudo docker start server3
 
 # sleep 5
 
