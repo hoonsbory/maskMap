d6d78246d26b3d2c7da60a45ea5a14a33e803fa0
unknown <tonymkcv@naver.com>
2020-03-19 21:23:32 +0900

    aws 메모리부족으로 컨테이너 1개로 줄임

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -7,25 +7,25 @@ sudo docker start server1
 
 sleep 5
 
-sudo docker stop server2
-sudo docker cp /home/ubuntu/travis/maskMap/target/maskmap-0.0.1-SNAPSHOT.jar server2:/
+# sudo docker stop server2
+# sudo docker cp /home/ubuntu/travis/maskMap/target/maskmap-0.0.1-SNAPSHOT.jar server2:/
 
 
-sudo docker start server2
+# sudo docker start server2
 
-sleep 5
+# sleep 5
 
-sudo docker stop server3
-sudo docker cp /home/ubuntu/travis/maskMap/target/maskmap-0.0.1-SNAPSHOT.jar server3:/
+# sudo docker stop server3
+# sudo docker cp /home/ubuntu/travis/maskMap/target/maskmap-0.0.1-SNAPSHOT.jar server3:/
 
 
-sudo docker start server3
+# sudo docker start server3
 
-sleep 5
+# sleep 5
 
-sudo docker cp /home/ubuntu/travis/maskMap/target/maskmap-0.0.1-SNAPSHOT.jar server4:/
+# sudo docker cp /home/ubuntu/travis/maskMap/target/maskmap-0.0.1-SNAPSHOT.jar server4:/
 
-sudo docker stop server4
+# sudo docker stop server4
 
-sudo docker start server4
+# sudo docker start server4
 
