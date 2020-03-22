11c1efc0e2dc756468d53384a5476b35cfa70218
unknown <tonymkcv@naver.com>
2020-03-22 21:58:30 +0900

    js 미적용

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -16,7 +16,7 @@ $(function () {
 	var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 	var options = { //지도를 생성할 때 필요한 기본 옵션
 		center: new kakao.maps.LatLng(37.498614, 127.041503), //지도의 중심좌표.
-		level: 4 //지도의 레벨(확대, 축소 정도)
+		level: 4 //지도의 레벨(확대, 축소 정도)ㅁ
 
 	};
 
