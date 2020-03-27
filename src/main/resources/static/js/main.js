d1b0a01b73cc8c61391bfca47f18ab690f78fdde
unknown <tonymkcv@naver.com>
2020-03-27 15:36:20 +0900

    아이패드 가로세로 변환 css대응

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,7 +4,6 @@ $(function () {
 	var result2;
 	var positionDeny = false;
 	var myPositionBtn = false;
-	var dragEventSWitch = true;
 	var myPositionMarker = [];
 	var menuSwitch = "stock";
 	var beforeOverlay = [];
@@ -13,11 +12,10 @@ $(function () {
 	var beforeClusterer = [];
 	var centerLat;
 	var centerLng;
-	var centerChBtn = false;
 	var firstLoad = false;
 	var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 	var options = { //지도를 생성할 때 필요한 기본 옵션
-		center: new kakao.maps.LatLng(37.498614, 127.041503), //지도의 중심좌표.
+		center: new kakao.maps.LatLng(37.580435, 127.217011), //지도의 중심좌표.
 		level: 4 //지도의 레벨(확대, 축소 정도)ㅁ
 
 	};
@@ -25,11 +23,11 @@ $(function () {
 	map.setMaxLevel(9);
 
 	var geoOptions = {
-		maximumAge: 10000,
-		enableHighAccuracy: false,
+		maximumAge: 10000, //위치정보 유효 시간
+		enableHighAccuracy: false, //정확한 위치 정보 끄기
 	}
 
-	function myPosition() {
+	function myPosition() {  //내 위치 불러오기
 		$(".loading").css("display", "block")
 		if (navigator.geolocation) {
 			// if (navigator.geolocation) {
@@ -42,8 +40,7 @@ $(function () {
 		}
 	}
 
-	function callback(pos) {
-		centerChBtn = true;
+	function callback(pos) {	//위치정보를 불러오면 실행되는 콜백함수
 		firstLoad = true;
 		positionDeny = false;
 		centerLat = pos.coords.latitude
@@ -64,16 +61,15 @@ $(function () {
 		});
 		myPositionMarker.push(marker);
 		firstLoad = true;
-		sendAddress();
 	}
 
-	function error(error) {
+	function error(error) {	//위치 정보를 불러오지 못할 때 실행되는 함수
 		positionDeny = true;
 		switch (error.code) {
-			case 1: alert("위치 제공을 차단하셨습니다. 브라우저 설정을 통해 위치설정을 허용해주세요.")
+			case 1: alert("위치 제공을 차단하셨습니다. 브라우저 설정을 통해 위치설정을 허용해주세요.") //사용자가 차단했을 시
 
 				break;
-			case 2: alert("위치 정보가 제공되지 않는 기기이거나 위치 제공 기능이 꺼져있습니다.")
+			case 2: alert("위치 정보가 제공되지 않는 기기이거나 위치 제공 기능이 꺼져있습니다.") //위치 기능이 꺼져있거나 지원하지 않는 기기일 시
 
 				break;
 
@@ -83,31 +79,41 @@ $(function () {
 		firstLoad = true;
 		sendAddress();
 	}
+
 	myPosition();
 	var clusterer = new kakao.maps.MarkerClusterer({
 		map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
 		averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
 		minLevel: 5 // 클러스터 할 최소 지도 레벨 
 	});
-	clusterer.setMinClusterSize(1);
+	clusterer.setMinClusterSize(1); //판매처가 1개여도 지도레벨이 5이상이면 클러스터러로 표현.
 
-	$(".btn_close").click(function () {
+
+
+	$(".btn_close").click(function () {	//오늘하루 보지 않기 버튼.
 		setCookieMobile("todayCookie", "done", 1);
 		$("#noticeClose").click();
 	});
+	$("#notice").click(function () {
+		$("#scrollImg").css("display", "block")
+	})
+	$(".modal-content").click(function () {
+		$("#scrollImg").fadeOut(400)
 
-	function setCookieMobile(name, value, expiredays) {
+	})
+	$(".modal-body").on("scroll", function () {
+		$("#scrollImg").fadeOut(400)
+	});
+
+	function setCookieMobile(name, value, expiredays) { //1일짜리 쿠키 생성.
 		var todayDate = new Date();
 		todayDate.setDate(todayDate.getDate() + expiredays);
 		document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";"
 	}
 	function getCookieMobile() {
 		var cookiedata = document.cookie;
-		if (cookiedata.indexOf("todayCookie=done") < 0) {
+		if (cookiedata.indexOf("todayCookie=done") < 0) {	//쿠키가 없으면 공지 띄움
 			$("#notice").click();
-		}
-		else {
-			$("#noticeClose").click();
 		}
 	}
 	getCookieMobile();
@@ -167,12 +173,9 @@ $(function () {
 		map.panTo(moveLatLon);
 	}
 
-	function closeOverlay() {
-		beforeOverlay[0].setMap(null);
-	}
 
 
-	// 주소로 좌표를 검색합니다
+	// 주소로 좌표를 검색
 	function addressSearch(address) {
 		centerChBtn = true;
 		var ps = new kakao.maps.services.Places();
@@ -180,29 +183,28 @@ $(function () {
 			alert('키워드를 입력해주세요!');
 			return false;
 		}
-		// 키워드로 장소를 검색합니다
+		// 키워드로 장소를 검색
 		ps.keywordSearch(address, placesSearchCB);
 
-		// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+		// 키워드 검색 완료 시 호출되는 콜백함수
 		function placesSearchCB(data, status, pagination) {
-			if (status == "ZERO_RESULT") {
+			if (status == "ZERO_RESULT") {	//데이터가 없을 시 안내
 				alert("찾으시는 장소가 없습니다")
 				return
 			}
 			if (status === kakao.maps.services.Status.OK) {
-				$("#address").blur();
+				$("#address").blur(); //인풋창 포커스아웃.
 				// 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-				// LatLngBounds 객체에 좌표를 추가합니다
+				// LatLngBounds 객체에 좌표를 추가
 				var bounds = new kakao.maps.LatLngBounds();
 
 				for (var i = 0; i < data.length; i++) {
 					bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
 				}
 
-				// 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+				// 검색된 장소 위치를 기준으로 지도 범위를 재설정
 				map.setLevel(4);
 				map.setBounds(bounds);
-				sendAddress();
 			}
 		}
 
@@ -269,6 +271,7 @@ $(function () {
 		}
 	}
 
+	//하단의 판매처 목록에 들어갈 태그들
 	function contentListFnc(i, statColor, count) {
 		var contentList = '<li id="storename' + count + '" class="storeLi">' +
 			'<div class="storeName">' + i.name + '</div>' +
@@ -278,6 +281,8 @@ $(function () {
 
 		return contentList
 	}
+
+	//오버레이 생성
 	function contentListFnc2(i, statColor) {
 		var content;
 		if (!i.avgTime) { //평균 입고 시간 기록이 없을 때.
@@ -321,10 +326,10 @@ $(function () {
 		return content;
 	}
 
-	//메인함수
+	//메인함수-판매처 정보 로드
 	function sendAddress() {
-		$(".loading").css("display", "block")
 		$(".storeLi").remove();
+		$(".loading").css("display", "block")
 		$("#centerChange").css("display", "none");
 		var latlng = [map.getCenter().getLat(), map.getCenter().getLng()]
 		var latlngJson = JSON.stringify(latlng)
@@ -335,29 +340,34 @@ $(function () {
 
 			contentType: 'application/json;charset=UTF-8',
 			success: function (data) {
+
 				result = JSON.parse(data);
 				//약국이 없을 때
-				if (Object.keys(result).length == 0) {
+				if (Object.keys(result).length == 0) { //객체 배열은 일반length가 안먹힘
 					$(".loading").css("display", "none")
 					var noStores = '<div class="storeLi" style="position : absolute; left : 50%; top : 50%; margin-left : -77px">근처에 약국이 없습니다</div>'
-					// result2.count = 0;
 					$(".storeList").append(noStores)
 					return
 
 				} else {
-					//전에 생성된 마커 삭제
-					// beforeMarker.forEach(i => {
 
-					// 	i.setMap(null);
-					// })
 
-					beforeMarker = [];
+					beforeMarker = []; //마커리스트와 오버레이 리스트 초기화
 					overlayList = [];
-					clusterer.clear();
+					clusterer.clear(); //전에 있던 마커들 삭제
+
 					result.forEach(i => {
+
+						//품절제외
+						if (excepted == true && i.remain_stat != "plenty" && i.remain_stat != "some" && i.remain_stat != "few") {
+							return true;
+						}
+						//가끔 위도경도가 없는 판매처 정보가 넘어온다..
+						if (!i.lat) return true;
+
 						if (!i.stock_at || JSON.stringify(i.stock_at) == "null") {
 							i.type = "입고 대기";
-							var distanceResult = Math.round(distance(centerLat, centerLng, i.lat, i.lng))
+							var distanceResult = Math.round(distance(centerLat, centerLng, i.lat, i.lng)) //위도 경도 미터로 변환
 							i.code = distanceResult;
 						} else {
 							//나중에 입고순으로 정렬을 해야하기 때문에 원본데이터인 stock_at는 냅두고 type에 데이터를 넣는다.
@@ -371,14 +381,11 @@ $(function () {
 							i.code = distanceResult;
 						}
 
-						if (excepted == true && i.remain_stat != "plenty" && i.remain_stat != "some" && i.remain_stat != "few") {
-							return true;
-						}
-						if (!i.lat) return true;
-
-
+						//시간을 몇 분 전 몇 시간 전으로 변환.
 						i.type = dateCal(i.type)
 
+
+						//빈 데이터들 처리
 						i.created_at = JSON.stringify(i.created_at) == "null" ? "입고 대기" : i.created_at;
 
 						//거리 데이터를 km, m로 변환
@@ -429,31 +436,21 @@ $(function () {
 							statColor = "#84898C"
 						}
 
-						var imageSrc = '/static/img/' + markerColor, // 마커이미지의 주소입니다    
-							imageSize = new kakao.maps.Size(28, 36), // 마커이미지의 크기입니다
-							imageOption = { offset: new kakao.maps.Point(15, 42) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-						// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+						//마커 생성
+						var imageSrc = '/static/img/' + markerColor,
+							imageSize = new kakao.maps.Size(28, 36),
+							imageOption = { offset: new kakao.maps.Point(15, 42) };
+
 						var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-							markerPosition = new kakao.maps.LatLng(i.lat, i.lng); // 마커가 표시될 위치입니다	
+							markerPosition = new kakao.maps.LatLng(i.lat, i.lng);
 
 						var marker = new kakao.maps.Marker({
-							// map: map,
 							position: markerPosition,
 							image: markerImage
 
 						});
-						marker.setTitle("마커")
 
-
-						// 데이터를 가져오기 위해 jQuery를 사용합니다
-						// 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
-						// 데이터에서 좌표 값을 가지고 마커를 표시합니다
-						// 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
-
-
-						// 클러스터러에 마커들을 추가합니다
-
-
+						//오버레이 생성
 						var content = contentListFnc2(i, statColor)
 
 						// 마커 위에 커스텀오버레이를 표시합니다
@@ -465,8 +462,9 @@ $(function () {
 						});
 						overlay.setZIndex(10)
 
-						//마커의 클릭이벤트 등록 전에 생성된 오버레이를 없애고 새 오버레이를 띄운다.
+						//마커의 클릭이벤트. 생성된 오버레이를 없애고 새 오버레이를 띄운다.
 						kakao.maps.event.addListener(marker, 'click', function () {
+							//좌표로 이동
 							panTo(i.lat, i.lng);
 							if (beforeOverlay[0]) {
 								beforeOverlay[0].setMap(null);
@@ -476,15 +474,18 @@ $(function () {
 							beforeOverlay.push(overlay);
 
 						});
-
+						//배열에 추가
 						beforeMarker.push(marker);
 						overlayList.push(overlay);
 					});
+
 					clusterer.addMarkers(beforeMarker);
 					beforeClusterer.push(clusterer)
 					//딥카피를 통해 정렬할 때 쓰일 객체를 생성.
 					result2 = JSON.parse(JSON.stringify(result))
 
+
+					//활성화된 정렬메뉴를 실행.
 					if (menuSwitch == "stock") {
 						recentStock();
 					} else {
@@ -515,12 +516,15 @@ $(function () {
 		$("#nearby").css("color", "#F1EEE6")
 		$("#nearby").css("border-bottom", "none")
 
+		//약국 없을시
 		if (Object.keys(result).length == 0) {
 			var noStores = '<div class="storeLi" style="position : absolute; left : 50%; top : 50%; margin-left : -77px">근처에 약국이 없습니다</div>'
 
 			$(".storeList").append(noStores)
 			return
 		}
+
+
 		result.forEach(i => {
 
 			if (!i.stock_at || JSON.stringify(i.stock_at) == "null") {
@@ -535,6 +539,7 @@ $(function () {
 			}
 		})
 
+		//입고순으로 정렬
 		result.sort(function (a, b) {
 			return b.type - a.type;
 		})
@@ -590,14 +595,13 @@ $(function () {
 						break;
 				}
 
+
 				var contentList = contentListFnc(i, statColor, count)
 
 				$(".storeList").append(contentList)
 
 				var content = contentListFnc2(i, statColor)
 
-				// 마커 위에 커스텀오버레이를 표시합니다
-				// 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
 				var position = new kakao.maps.LatLng(
 
 					i.lat, i.lng
@@ -609,26 +613,26 @@ $(function () {
 				});
 				overlay.setZIndex(10)
 
+				//정렬된 목록의 클릭 이벤트. 확대 레벨이 5이상인 경우 3레벨로 확대 후 오버레이 생성, 5레벨 이상일 때는 클러스터러로 표현되기 때문에 확대해서 마커 단위로 바꿔준 후 오버레이 생성.
 				$("#storename" + count).click(function () {
 
 					if (map.getLevel() > 4) {
 						map.setLevel(3);
 					}
-					dragEventSWitch = false;
-					if (dragEventSWitch == false) panTo(i.lat, i.lng);
+					panTo(i.lat, i.lng);
 					if (beforeOverlay[0]) {
 						beforeOverlay[0].setMap(null);
 					}
 					beforeOverlay = [];
 					overlay.setMap(map);
 					beforeOverlay.push(overlay);
-					dragEventSWitch = true;
 				})
 
 				count++;
 				divCount++;
 				counter++;
 				breakCount++;
+				//리스트 11개까지만 생성. 
 				if (Object.keys(result).length < 11 && breakCount == Object.keys(result).length) return true;
 				if (breakCount == 11) {
 
@@ -642,6 +646,7 @@ $(function () {
 		$(".scrollDiv").on("scroll", function () {
 			var scrollHeight = $(".storeList").height();
 			var scrollPosition = $(".scrollDiv").height() + $(".scrollDiv").scrollTop();
+			//스크롤이 바닥에서 50px 미만으로 들어오면 새로운 리스트 생성
 			if (scrollPosition > scrollHeight - 50 && divCount != Object.keys(result).length) {
 				scrollContent();
 			}
@@ -651,7 +656,7 @@ $(function () {
 		$(".slideUp").scrollTop(0);
 	}
 
-	//거리순 정렬
+	//거리순 정렬 내용은 거리순 정렬빼고는 위와 같다.
 	function nearby() {
 		if (positionDeny == true) {
 			alert("위치 제공을 차단하셨거나 지원하지 않습니다.")
@@ -756,22 +761,17 @@ $(function () {
 					clickable: true
 				});
 				overlay.setZIndex(10)
-				var close = document.createElement('div')
-				close.className = "close"
-				close.onclick = closeOverlay
 				$("#storename" + count).click(async function () {
 					if (map.getLevel() > 4) {
 						map.setLevel(3);
 					}
-					dragEventSWitch = false;
-					if (dragEventSWitch == false) panTo(i.lat, i.lng);
+					panTo(i.lat, i.lng);
 					if (beforeOverlay[0]) {
 						beforeOverlay[0].setMap(null);
 					}
 					beforeOverlay = [];
 					await overlay.setMap(map);
 					beforeOverlay.push(overlay);
-					dragEventSWitch = true;
 
 				})
 				count++;
@@ -816,6 +816,7 @@ $(function () {
 	})
 
 
+	//새로고침 버튼
 	$("#refresh").click(function () {
 		$("#refresh").css("background-color", "#0f4c81");
 		sendAddress();
@@ -825,6 +826,7 @@ $(function () {
 
 	})
 
+	//내 위치 버튼 , 눌린 상태에서 누르면 마커가 제거되면서 비활성 아이콘으로 바뀜.
 	$("#myPosition").click(function () {
 		if (myPositionBtn == false) {
 			myPositionBtn = true
@@ -848,9 +850,12 @@ $(function () {
 		}
 	})
 
+	//인풋창 돋보기 클릭시 검색.
 	$("#searchIcon").click(function () {
 		addressSearch($("#address").val())
 	})
+
+	//품절제외 버튼.
 	$("#exceptedOn").click(function () {
 		excepted = true
 		if (beforeOverlay[0]) beforeOverlay[0].setMap(null)
@@ -861,13 +866,10 @@ $(function () {
 		if (beforeOverlay[0]) beforeOverlay[0].setMap(null)
 		sendAddress();
 	})
-	$('#slideContentDown').on('click', function () {
-		$('.slideDown').slideDown();
-	});
-	$('#slideContentUp').on('click', function () {
-		$('.slideDown').slideUp();
-	});
 
+
+
+	//목록열기 버튼. 아이패드 크기 이상의 기기에서는 목록의 width값이 100%가 아니라서 양옆에 버튼과 재고 알림박스는 슬라이드 기능 off
 	$('#reverseSlideContentUp').on('click', function () {
 		if ($('#reverseSlideContentUp').text() == "목록 열기") {
 			if (document.getElementsByClassName("storeList")[0].childElementCount == 0) {
@@ -875,7 +877,7 @@ $(function () {
 			}
 			$('.slideUp').slideDown();
 			$('#reverseSlideContentUp').html("목록 닫기")
-			if (window.innerWidth < 1040) {
+			if (window.innerWidth < 1024) {
 				$('#myPosition').animate({ bottom: "42%" }, 400);
 				$('.remainInfo').animate({ bottom: "42%" }, 400);
 				var height = window.innerHeight * (42 / 100) + 42;
@@ -889,7 +891,7 @@ $(function () {
 		} else {
 			$('.slideUp').slideUp();
 			$('#myPosition').animate({ bottom: "18px" }, 400);
-			if (window.innerWidth < 1040) {
+			if (window.innerWidth < 1024) {
 				$('#refresh').animate({ bottom: "60px" }, 400);
 			}
 			$('#reverseSlideContentUp').animate({ bottom: "18px" }, 400);
@@ -899,6 +901,7 @@ $(function () {
 	});
 
 
+	//지도 클릭 이벤트 , 오버레이 제거.
 	var clickHandler = function (e) {
 
 		if (beforeOverlay[0]) beforeOverlay[0].setMap(null)
@@ -907,7 +910,7 @@ $(function () {
 
 	kakao.maps.event.addListener(map, 'zoom_changed', function () {
 
-		// 지도의 현재 레벨을 얻어옵니다
+		// 지도의 현재 레벨을 얻어온다. 확대나 축소시에 레벨이 5이상이면 오버레이 제거. 클러스터러로 표시되기 때문.
 		if (map.getLevel() > 4) {
 
 			if (beforeOverlay[0]) beforeOverlay[0].setMap(null)
@@ -915,9 +918,33 @@ $(function () {
 
 
 	});
+	//카카오맵의 타일이 로드될 때마다 판매처 정보 로드.
 	kakao.maps.event.addListener(map, 'tilesloaded', function () {
 		if (firstLoad == true) sendAddress();
 
 	});
+
+	$(window).resize(function () {
+		// width값을 가져오기
+		var width_size = window.innerWidth;
+
+		// 아이패드 가로로 전환시 css 크기에 맞게 재적용.
+		if (width_size >= 1024) {
+			$('#refresh').css("bottom", "85px");
+			$('#myPosition').css("bottom", "18px");
+			$(".remainInfo").css("bottom", "18px")
+		}
+		if (width_size >= 1024 && $('#reverseSlideContentUp').text() == "목록 닫기") {
+			$('#reverseSlideContentUp').css("bottom", "38%")
+		}
+		// 아이패드 세로로 전환시 css 크기에 맞게 재적용.
+		if (width_size < 1024 && $('#reverseSlideContentUp').text() == "목록 닫기") {
+			$('#reverseSlideContentUp').css("bottom", "42%")
+			var height = window.innerHeight * (42 / 100) + 42;
+			$('#refresh').css("bottom", height + "px");
+			$('#myPosition').css("bottom", "42%");
+			$(".remainInfo").css("bottom", "42%")
+		}
+	})
 
 });
